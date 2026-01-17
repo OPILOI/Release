3f4154dcac993ed1c5c6082a154df50a2a83ee87
OPILOI <Maybe.hook@proton.me>
2026-01-17 15:43:16 +0100

    Update pc.lua

--- PublicScript/pc.lua
+++ PublicScript/pc.lua
@@ -1,12 +1,15 @@
-if not (_G.SESSION_TOKEN and _G.Processor) or 
-   type(_G.SESSION_TOKEN) ~= "number" or 
-   type(_G.Processor) ~= "number" then
-    game.Players.LocalPlayer:Kick("Tampering")
+if not (_G.SESSION_TOKEN and _G.Processor) or type(_G.SESSION_TOKEN) ~= "number" or type(_G.Processor) ~= "number" then
+    game.Players.LocalPlayer:Kick("Tampering (Final PC Check)")
     return
 end
 
-warn("Loading pc..")
+_G.SESSION_TOKEN = nil
+_G.Processor = nil
+shared.SESSION_TOKEN = nil
+shared.Processor = nil
+
+print("Loading pc..")
 
 
 
-warn("Loaded pc!")
+print("Loaded pc!")
