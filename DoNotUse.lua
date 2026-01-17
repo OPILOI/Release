d34e51c8dc0cc46127893a44cce7bc01a66ba1ad
OPILOI <Maybe.hook@proton.me>
2026-01-17 15:36:15 +0100

    Update DoNotUse.lua

--- DoNotUse.lua
+++ DoNotUse.lua
@@ -1,5 +1,21 @@
 if not (_G.SESSION_TOKEN and _G.Processor) or type(_G.SESSION_TOKEN) ~= "number" or type(_G.Processor) ~= "number" then
-    game.Players.LocalPlayer:Kick("Tampering")
+    game.Players.LocalPlayer:Kick("Tampering (Step 5 Error)")
+    return
+end
+
+local _TOKEN = nil
+local _Start = tick()
+while (tick() - _Start) < 10 do
+    if _G.SYNX_TOKEN then
+        _TOKEN = _G.SYNX_TOKEN
+        _G.SYNX_TOKEN = nil
+        break
+    end
+    task.wait(0.2)
+end
+
+if not _TOKEN or type(_TOKEN) ~= "number" then
+    game.Players.LocalPlayer:Kick("Tampering (Sync Failed)")
     return
 end
 
@@ -8,13 +24,17 @@ local _TARGET = nil
 pcall(function() _TARGET = BZAGZFIG end)
 
 local _OK = false
+local _VStart = tick()
 if _R and _TARGET then
-    local success, r = pcall(function()
-        return _R({Url = "https://opiloi.github.io/WEBSITE/?t="..tick(), Method = "GET", Timeout = 5})
-    end)
-    if success and r and r.Body then
-        local code = r.Body:match('id="codebox"%s+value="(.-)"')
-        if code == _TARGET then _OK = true end
+    while (tick() - _VStart) < 10 do
+        local success, r = pcall(function()
+            return _R({Url = "https://opiloi.github.io/WEBSITE/?t="..tick(), Method = "GET", Timeout = 5})
+        end)
+        if success and r and r.Body then
+            local code = r.Body:match('id="codebox"%s+value="(.-)"')
+            if code == _TARGET then _OK = true break end
+        end
+        task.wait(1)
     end
 end
 
@@ -23,13 +43,15 @@ if not _OK then
     return
 end
 
+warn("Validated session..")
+
 _G.SESSION_TOKEN = nil
 _G.Processor = nil
 shared.SESSION_TOKEN = nil
 shared.Processor = nil
 
-local UIS = game:GetService("UserInputService")
-if UIS.TouchEnabled and not UIS.KeyboardEnabled then
+local UserInputService = game:GetService("UserInputService")
+if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OPILOI/Release/refs/heads/main/PublicScript/mobile.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OPILOI/Release/refs/heads/main/PublicScript/pc.lua"))()
