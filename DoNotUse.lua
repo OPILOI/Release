8496093ba9ff5d949eebf9b8a40ace3f0fa18ca3
OPILOI <Maybe.hook@proton.me>
2026-01-17 13:41:52 +0100

    Update DoNotUse.lua

--- DoNotUse.lua
+++ DoNotUse.lua
@@ -48,5 +48,5 @@ local UserInputService = game:GetService("UserInputService")
 if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OPILOI/Release/refs/heads/main/PublicScript/mobile.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MaybeHookUser/Mabye-Hook/refs/heads/main/pc.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OPILOI/Release/refs/heads/main/PublicScript/pc.lua"))()
 end
