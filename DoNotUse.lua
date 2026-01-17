bb793abc4dc46d6d93ba784949a6efcda57aba40
OPILOI <Maybe.hook@proton.me>
2026-01-17 11:02:22 +0100

    Update DoNotUse.lua

--- DoNotUse.lua
+++ DoNotUse.lua
@@ -6,3 +6,21 @@ if not _TOKEN or type(_TOKEN) ~= "number" then
     game.Players.LocalPlayer:Kick("Connection not secure: Handshake Failed")
     return
 end
+
+
+local request=request or http_request or syn.request
+local ok=false
+if request and BZAGZFIG then
+    local r=request({Url="https://opiloi.github.io/WEBSITE/",Method="GET"})
+    if r and r.Body then
+        local code=r.Body:match('id="codebox"%s+value="(.-)"')
+        if code and code==BZAGZFIG then
+            ok=true
+        end
+    end
+end
+if not ok then
+    game:GetService("Players").LocalPlayer:Kick()
+end
+
+print("up")
