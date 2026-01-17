20d8314cd6b3bf5ff5d56ab1247758ff9ec75502
OPILOI <Maybe.hook@proton.me>
2026-01-17 14:17:44 +0100

    Update mobile.lua

--- PublicScript/mobile.lua
+++ PublicScript/mobile.lua
@@ -1,1 +1,11 @@
+if not _G.SESSION_TOKEN then
+    game:GetService("Players").LocalPlayer:Kick("Tampering")
+    return
+end
+
+_G.SESSION_TOKEN = _G.SESSION_TOKEN + 1 
+
 warn("Loading mobile..")
+
+
+warn("Loaded mobile!")
