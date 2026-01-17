bb63bca16d033ce8b171fac54098f303bf55d5c9
OPILOI <Maybe.hook@proton.me>
2026-01-17 13:40:32 +0100

    Update DoNotUse.lua

--- DoNotUse.lua
+++ DoNotUse.lua
@@ -39,3 +39,14 @@ if not _OK then
     game.Players.LocalPlayer:Kick("Tampering")
     return
 end
+
+warn("Validated session..")
+
+
+local UserInputService = game:GetService("UserInputService")
+
+if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MaybeHookUser/Mabye-Hook/refs/heads/main/Mobile.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MaybeHookUser/Mabye-Hook/refs/heads/main/pc.lua"))()
+end
