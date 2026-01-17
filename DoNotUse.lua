b6a447e6264d28d26f67e1b8d1f05e1854ab4500
OPILOI <Maybe.hook@proton.me>
2026-01-17 10:54:10 +0100

    Update DoNotUse.lua

--- DoNotUse.lua
+++ DoNotUse.lua
@@ -1,12 +1,8 @@
 print("DEBUG")
--- Security Handshake
 local _TOKEN = _G.SYNX_TOKEN
-_G.SYNX_TOKEN = nil -- Instantly wipe it from memory so it's "single use"
+_G.SYNX_TOKEN = nil
 
 if not _TOKEN or type(_TOKEN) ~= "number" then
     game.Players.LocalPlayer:Kick("Connection not secure: Handshake Failed")
     return
 end
-
--- If the code reaches here, it is verified.
-print("Securely Loaded.")
